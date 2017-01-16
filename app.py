896782b8b19d43a4926b9899cc1867c77f1acd26
Afischbacher <ital-andre95@live.com>
2017-01-16 16:44:39 -0500

    added functionality to app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 # Kik bot messenger using Python 3, Flask, Gunicorn and Jinja 2
 # By: Andre Fischbacher
-import json
-import requests
+
 from flask import Flask, request, Response
 import os
 from kik import KikApi, Configuration
@@ -13,6 +12,7 @@ kik = KikApi("afischbacher95", "2dd4a60c-287b-4b95-8252-fd8dfe577759")
 config = Configuration(webhook='https://kik-bot-messenger.herokuapp.com/incoming')
 kik.set_configuration(config)
 
+global kik_chat_id, user
 
 @app.route('/incoming', methods=['POST'])
 def incoming():
@@ -22,6 +22,10 @@ def incoming():
     messages = messages_from_json(request.json['messages'])
 
     for message in messages:
+
+        kik_chat_id = message.chat_id
+        user = message.from_user
+
         if isinstance(message, TextMessage):
             kik.send_messages([
                 TextMessage(
@@ -30,6 +34,15 @@ def incoming():
                     body=message.body
                 )
             ])
+
+
+    kik.send_broadcast([
+        TextMessage(
+            to=user,
+            chat_id=kik_chat_id,
+            body="Hey Im Andre !"
+        )
+    ])
 
     return Response(status=200)
 
