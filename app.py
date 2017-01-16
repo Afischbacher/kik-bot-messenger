3141e26210840df751150d1332af0ce17667f2fd
Afischbacher <ital-andre95@live.com>
2017-01-16 15:08:10 -0500

    added kik api and added the "/incoming" url for the interaction between the api and kik

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
-from flask import Flask
+from flask import Flask, request, Response
+import os
+from kik import KikApi, Configuration
+from kik.messages import messages_from_json, TextMessage
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return 'Hello World'
+kik = KikApi("afischbacher95", "2dd4a60c-287b-4b95-8252-fd8dfe577759")
+config = kik.get_configuration()
+
+kik.set_configuration(Configuration(webhook='https://kik-bot-messenger.herokuapp.com/incoming'))
+
+
+@app.route('/incoming', methods=['POST'])
+def incoming():
+    return "Hello Incoming"
+
+
+@app.route("/", methods=['GET'])
+def hello():
+    return "Hello World"
+
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
