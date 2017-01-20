2a7cb497090fd282d8d906900a3386ec694ed709
Afischbacher <ital-andre95@live.com>
2017-01-19 20:51:19 -0500

    Added additional api's and edited the beginning message

--- app.py
+++ app.py
@@ -4,7 +4,8 @@
 from flask import Flask, request, Response
 import os
 from kik import KikApi, Configuration
-from kik.messages import messages_from_json, TextMessage
+from kik.messages import messages_from_json, TextMessage, StartChattingMessage, ScanDataMessage, LinkMessage, \
+    DeliveryReceiptMessage, ReadReceiptMessage, VideoMessage
 
 app = Flask(__name__)
 
@@ -12,7 +13,6 @@ kik = KikApi("afischbacher95", "2dd4a60c-287b-4b95-8252-fd8dfe577759")
 config = Configuration(webhook='https://kik-bot-messenger.herokuapp.com/incoming')
 kik.set_configuration(config)
 
-global kik_chat_id, user
 
 @app.route('/incoming', methods=['POST'])
 def incoming():
@@ -23,33 +23,21 @@ def incoming():
 
     for message in messages:
 
-        kik_chat_id = message.chat_id
-        user = message.from_user
-
-        if isinstance(message, TextMessage):
+        if isinstance(message, StartChattingMessage):
             kik.send_messages([
                 TextMessage(
                     to=message.from_user,
                     chat_id=message.chat_id,
-                    body=message.body
+                    body="Hello Welcome to my chat bot!"
                 )
             ])
-
-
-    kik.send_broadcast([
-        TextMessage(
-            to=user,
-            chat_id=kik_chat_id,
-            body="Im Andre, Nice to meet you!"
-        )
-    ])
 
     return Response(status=200)
 
 
 @app.route("/", methods=['GET'])
 def hello():
-    return "<h1> Hello Welcome To My Kik Bot Messenger </h1>"
+    return "<h1> Hello Welcome To My Kik Bot Messenger, Check out my bot at @afischbacher95 </h1>"
 
 
 if __name__ == "__main__":
