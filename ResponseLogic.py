99f40b95c6686b972b6138d3a9c1c937d6623054
Afischbacher <ital-andre95@live.com>
2017-01-23 17:02:32 -0500

    added textlogic file to seperate text logic from response logic.

--- ResponseLogic.py
+++ ResponseLogic.py
@@ -1,9 +1,10 @@
 import re
+import random
+import TextLogic
 
 
 def route_response_logic(recieved_message):
-    if re.search("[[Hh]ey", recieved_message, re.IGNORECASE):
-        return "Hey I'm Andre, this is my chat bot, hope you like it! To get started ask me a question" \
-           "(I might not know all the answers to them though)"
+    if re.search('\bWhats\b | \bHey\b | \bHello\b | \bHi\b', recieved_message, flags=re.I | re.X):
+        return "{0}, ask me a question to get to know me".format(random.choice(TextLogic.greeting_options))
     else:
         return "I am not quite sure I have the answer to that question, ask me later ! Sorry"
---
+++ TextLogic.py
@@ -0,0 +1,5 @@
+# The response text lists and tuples for the chat bot to use
+
+greeting_options = ["Hey whats up !", "What is going on today ?", "Just me here, looking cool and all", "Hello", "Hey",
+                    "hey", "whats up", "sup bro", "sup", "hey cool cat",
+                    "What crackalackin ?... is that even a thing anymore ?", "Whats good ? "]
