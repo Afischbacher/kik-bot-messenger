44763cdd848cfabf1126526e5f9dc1e291b49de7
Afischbacher <ital-andre95@live.com>
2017-01-22 23:06:35 -0500

    added an "I dont know" response to the testing structure

--- ResponseLogic.py
+++ ResponseLogic.py
@@ -5,3 +5,5 @@ def route_response_logic(recieved_message):
     if re.search("[[Hh]ey", recieved_message, re.IGNORECASE):
         return "Hey I'm Andre, this is my chat bot, hope you like it! To get started ask me a question" \
            "(I might not know all the answers to them though)"
+    else:
+        return "I am not quite sure I have the answer to that question, ask me later ! Sorry"
