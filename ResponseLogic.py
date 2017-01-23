9095ccd6f9873cb1841d95a9c22716a2296ff440
Afischbacher <ital-andre95@live.com>
2017-01-23 17:30:29 -0500

    changed the random logic for the greeting response

--- ResponseLogic.py
+++ ResponseLogic.py
@@ -1,10 +1,12 @@
 import re
 import random
-import TextLogic
+from TextLogic import greeting_options
+import math
 
 
 def route_response_logic(recieved_message):
-    if re.search('\bWhats\b | \bHey\b | \bHello\b | \bHi\b', recieved_message, flags=re.I | re.X):
-        return "{0}, ask me a question to get to know me".format(random.choice(TextLogic.greeting_options))
+    if re.search('\bWhats\b | \bHey\b | \bHello\b | \bHi\b | \bGr(.*?)s\b', recieved_message, flags=re.I | re.X):
+        return "{0}, ask me a question to get to know me".format(
+            greeting_options[math.floor(random.random * len(greeting_options))])
     else:
         return "I am not quite sure I have the answer to that question, ask me later ! Sorry"
--- TextLogic.py
+++ TextLogic.py
@@ -1,5 +1,6 @@
 # The response text lists and tuples for the chat bot to use
 
+
 greeting_options = ["Hey whats up !", "What is going on today ?", "Just me here, looking cool and all", "Hello", "Hey",
                     "hey", "whats up", "sup bro", "sup", "hey cool cat",
                     "What crackalackin ?... is that even a thing anymore ?", "Whats good ? "]
