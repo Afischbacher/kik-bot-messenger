162bcbb5f33cca980a61f85aba11d1cac91be015
Afischbacher <ital-andre95@live.com>
2017-01-23 17:40:12 -0500

    altered the selection method

--- TextLogic.py
+++ TextLogic.py
@@ -3,4 +3,4 @@
 
 greeting_options = ["Hey whats up !", "What is going on today ?", "Just me here, looking cool and all", "Hello", "Hey",
                     "hey", "whats up", "sup bro", "sup", "hey cool cat",
-                    "What crackalackin ?... is that even a thing anymore ?", "Whats good ? "]
+                    "What cracking ?... is that even a thing anymore ?", "Whats good ? "]
