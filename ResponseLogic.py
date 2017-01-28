4d5adc4252937eafca21dfc054f8d581c032508b
Afischbacher <ital-andre95@live.com>
2017-01-27 20:04:19 -0500

    adjusted the inital SQL query in the database connection file

--- ResponseLogic.py
+++ ResponseLogic.py
@@ -1,7 +1,7 @@
 import sqlite3
 from flask import g
 
-rand_query = "SELECT `Answers` FROM Answers, Keywords WHERE Answers.Keyword_ID = Keywords.ID AND `Keywords` LIKE LOWER('%' || ? || '%') ORDER BY RANDOM() LIMIT 1;"
+rand_query = "SELECT `Answers` FROM Answers, Keywords WHERE Answers.ID = Keywords.ID AND `Keywords` LIKE LOWER('%' || ? || '%') ORDER BY RANDOM() LIMIT 1;"
 
 
 def sql_conn():
