e33b86639e2047d003372707245392b4106f7cbe
Afischbacher <ital-andre95@live.com>
2017-01-31 13:53:40 -0500

    added additional response logic and added SQL data into the database

--- ResponseLogic.py
+++ ResponseLogic.py
@@ -4,22 +4,20 @@ from flask import g
 global conn, cursor
 
 
-def sql_conn():
+def route_response_logic(recieved_message):
     PATH = 'ReverseIndex.db'
     db = getattr(g, '__database', None)
 
     if db is None:
         conn = g.__database = sqlite3.connect(PATH)
         cursor = conn.cursor()
-
-
-def route_response_logic(recieved_message):
     msg_array = recieved_message.split()
 
     for msg in msg_array:
         sql = "SELECT `Answers` FROM Answers, Keywords WHERE Answers.ID = Keywords.ID AND" \
               " `Keywords` LIKE LOWER('%?%') ORDER BY RANDOM() LIMIT 1;"
-        cursor.execute(sql, msg_array.index(msg))
-        answer = cursor.fetchone().__str__()
-
+        cursor.execute(sql, msg)
+        if cursor.fetchone():
+            answer = cursor.fetchone().__str__()
+        break
     return answer
