522de3de39c41e30b04a689634b1754e57e7c2f8
Afischbacher <ital-andre95@live.com>
2017-01-31 13:11:00 -0500

    editied the SQL query and returned the response from the user

--- ResponseLogic.py
+++ ResponseLogic.py
@@ -1,7 +1,7 @@
 import sqlite3
 from flask import g
 
-
+global conn, cursor
 
 
 def sql_conn():
@@ -9,17 +9,17 @@ def sql_conn():
     db = getattr(g, '__database', None)
 
     if db is None:
-        global conn, cursor
         conn = g.__database = sqlite3.connect(PATH)
         cursor = conn.cursor()
-        sql = "SELECT `Answers` FROM Answers, Keywords WHERE Answers.ID = Keywords.ID AND `Keywords` LIKE LOWER('%' || ? || '%') ORDER BY RANDOM() LIMIT 1;"
-
 
 
 def route_response_logic(recieved_message):
     msg_array = recieved_message.split()
-    for msg in msg_array:
-        sql = "SELECT `Answers` FROM Answers, Keywords WHERE Answers.ID = Keywords.ID AND `Keywords` LIKE LOWER('%' || ? || '%') ORDER BY RANDOM() LIMIT 1;"
-        cursor.execute(sql,msg_array.index(msg))
 
-    return cursor.fetchone()
+    for msg in msg_array:
+        sql = "SELECT `Answers` FROM Answers, Keywords WHERE Answers.ID = Keywords.ID AND" \
+              " `Keywords` LIKE LOWER('%?%') ORDER BY RANDOM() LIMIT 1;"
+        cursor.execute(sql, msg_array.index(msg))
+        answer = cursor.fetchone().__str__()
+
+    return answer
