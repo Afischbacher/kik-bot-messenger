b8716b527a29631dd66cc5493f39702b2055bc76
Afischbacher <ital-andre95@live.com>
2017-01-31 14:28:22 -0500

    Renamed the file extension for database and editied resposne logic

--- ResponseLogic.py
+++ ResponseLogic.py
@@ -5,19 +5,19 @@ global conn, cursor, answer
 
 
 def route_response_logic(recieved_message):
-    PATH = 'ReverseIndex.db'
     db = getattr(g, '__database', None)
 
     if db is None:
-        conn = g.__database = sqlite3.connect(PATH)
+        conn = g.__database = sqlite3.connect('ReverseIndex')
         cursor = conn.cursor()
-    msg_array = recieved_message.split()
 
-    for msg in msg_array:
-        sql = "SELECT `Answers` FROM Answers, Keywords WHERE Answers.ID = Keywords.ID AND" \
-              " `Keywords` LIKE LOWER('%?%') ORDER BY RANDOM() LIMIT 1;"
-        cursor.execute(sql, msg)
+    msg_list = recieved_message.split()
+
+    for msg in msg_list:
+        cursor.execute(
+            "SELECT Answers FROM Answers, Keywords WHERE Answers.ID = Keywords.ID AND Keywords LIKE LOWER('%?%') ORDER BY RANDOM() LIMIT 1;",
+            msg)
         if cursor.fetchone():
             answer = cursor.fetchone()
-        break
+
     return answer
