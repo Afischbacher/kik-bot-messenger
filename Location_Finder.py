31c4856be04e5ff212e88777f5c3b72acfdbcfda
Afischbacher <ital-andre95@live.com>
2017-02-04 19:10:51 -0500

    made certain variable changes and added proper response string interpolation

--- Location_Finder.py
+++ Location_Finder.py
@@ -6,5 +6,5 @@ def get_location():
     resp = urllib.request.urlopen("http://ip-api.com/json")
     ip_address = json.load(resp)
     location_country = ip_address['country']
-    location_city = ip_address['region']
-    return location_country + " " + location_city
+    location_region = ip_address['region']
+    return "Looks like your in the country of {0} and the region of {1}".format(location_country,location_region)
