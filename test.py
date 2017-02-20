92df22ccdbd0c2ac466c320071e8aa972c2d885b
Afischbacher <ital-andre95@live.com>
2017-02-20 13:53:20 -0500

    Revert "adding the updated workspace.xml file and a test.py file for console printing the database responses for the kik-bot-messenger"
    This reverts commit b0ef8279b476cee8a07f26891f8ed8d81a2f478d.

--- test.py
+++ test.py
@@ -1,33 +1,20 @@
 import sqlite3
-from location_finder import get_location
 
 
 def logic():
-    conn = sqlite3.connect('ResponseDatabase')
+    conn = sqlite3.connect('ReverseIndex')
     cursor = conn.cursor()
-
-    resp = "Location"
-    msg_list = resp.split()
-
-    if "location" in resp or "Location" in resp:
-        print(get_location())
-        return
+    msg_list = "asdfasd".split()
 
     for msg in msg_list:
         cursor.execute(
-            "SELECT Answers FROM Answers, Keywords WHERE Answers.ID = Keywords.ID AND Keywords LIKE '% ' || LOWER(?) || ' %' ORDER BY RANDOM() LIMIT 1;",
+            "SELECT Answers FROM Answers, Keywords WHERE Answers.ID = Keywords.ID AND Keywords LIKE LOWER('%' || ? || '%') ORDER BY RANDOM() LIMIT 1;",
             (msg,))
         row = cursor.fetchone()
         if row is not None:
             return row[0]
-
-    cursor.execute(
-        "SELECT ConnectingResponse FROM `Unknown Answers` ORDER BY RANDOM() LIMIT 1;"
-    )
-
-    connecting_resp = cursor.fetchone()
-    return connecting_resp[0]
-
+        else:
+            return "Sorry I can not answer that question right, now I am still getting smarter"
 
 
 print(logic())
